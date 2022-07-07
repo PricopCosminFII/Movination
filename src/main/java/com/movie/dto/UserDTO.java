42f234b6b7355d88b7db405ba33a6d08cb93f1b8
Madalina-Lavinia Dulhac-Livanu <madalina.dulhac@youngculture.com>
2022-07-07 09:57:37 +0000

    Modeled User Entity, Role Entity, DTOs

---
+++ src/main/java/com/movie/dto/UserDTO.java
@@ -0,0 +1,24 @@
+package com.movie.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private Long id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    private String password;
+}
